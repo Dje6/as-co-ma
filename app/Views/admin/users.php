80982e14d7530075ef9aab1b61b212592318381b
Simonesque <savalle.simon@gmail.com>
2016-12-16 22:21:50 +0100

    finition de lintegration du HTML dans la vu Admin/Users , j'ai preparer les span pour acceuillir les erreur et installer les balise pour que les modification soi faite en gardan le meme aspect

--- app/Views/admin/users.php
+++ app/Views/admin/users.php
@@ -16,79 +16,85 @@ echo '<h1 class="titreusers">Vos informations personnelles</h1><br/>';
 }
 
 if(isset($donnee)){//si la base de donnee retourne des information , array comme string
-  if(is_array($donnee)){//si la base de donnee retourne un array
+  if(is_array($donnee)){ //si la base de donnee retourne un array
+    ?>
+      <div class="container">
+        <div class="row alignement">
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-6 toppad" >
+            <div class="panel panel-info">
+              <div class="panel-heading"><?php
+                echo '<h3 classe"userpseudo">Votre Pseudo  : '.$donnee['pseudo'].'</h3><br/>'; ?>
+              </div>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-md-12 col-lg-12 " align="center">
+                    <!-- <img alt="User Pic" src="<?php //echo $donnee['avatar'] ; ?>" class="img-circle img-responsive"> -->
+                    <img alt="User Pic" src="https://cdn.pixabay.com/photo/2012/04/13/21/07/user-33638_960_720.png"
+                    class="img-circle img-responsive col-md-3">
+                    <div class=" col-md-9 col-lg-9 cartevisite">
+                      <form method="POST" action="<?php echo $this->url('admin_monCompte_edition_post') ; ?>">
+                        <table class="table table-user-information">
+                          <tbody><?php
+                            if(isset($edition) && !isset($acces)){ ?>
 
-    if(isset($edition) && !isset($acces)){
-          echo '<form method="POST" action="'.$this->url('admin_monCompte_edition_post').'">';
-          echo '<label for="pseudo">pseudo</label><br>';
-          echo '<input type="text" name="pseudo" value="'.$donnee['pseudo'].'"><br/>';
+                              <?php if(isset($error['pseudo'])){ echo '<tr><td>Erreur </td><td>'.$error['pseudo'].'</td></tr>'; } ?>
+                              <tr><td><label for="pseudo">pseudo</label></td>
+                              <td><input type="text" name="pseudo" value="<?php echo $donnee['pseudo'] ; ?>"><td/></tr>
 
-          echo '<label for="mail">mail</label><br>';
-          echo '<input type="text" name="mail" value="'.$donnee['mail'].'"><br/>';
+                              <?php if(isset($error['mail'])){ echo '<tr><td>Erreur </td><td>'.$error['mail'].'</td></tr>'; } ?>
+                              <tr><td><label for="mail">mail</label></td>
+                              <td><input type="text" name="mail" value="<?php echo $donnee['mail'] ; ?>"><td/></tr>
 
-          echo '<label for="adresse">adresse</label><br>';
-          echo '<input type="text" name="adresse" value="'.$donnee['adresse'].'"><br/>';
+                              <?php if(isset($error['adresse'])){ echo '<tr><td>Erreur </td><td>'.$error['adresse'].'</td></tr>'; } ?>
+                              <tr><td><label for="adresse">adresse</label></td>
+                              <td><input type="text" name="adresse" value="<?php echo $donnee['adresse'] ; ?>"><td/></tr>
 
-          echo '<label for="code_postal">code postal</label><br>';
-          echo '<input type="text" name="code_postal" value="'.$donnee['code_postal'].'"><br/>';
+                              <?php if(isset($error['code_postal'])){ echo '<tr><td>Erreur </td><td>'.$error['code_postal'].'</td></tr>'; } ?>
+                              <tr><td><label for="code_postal">code postal</label></td>
+                              <td><input type="text" name="code_postal" value="<?php echo $donnee['code_postal'] ; ?>"><td/></tr>
 
-          echo '<label for="ville">ville</label><br>';
-          echo '<input type="text" name="ville" value="'.$donnee['ville'].'"><br/>';
+                              <?php if(isset($error['ville'])){ echo '<tr><td>Erreur </td><td>'.$error['ville'].'</td></tr>'; } ?>
+                              <tr><td><label for="ville">ville</label></td>
+                              <td><input type="text" name="ville" value="<?php echo $donnee['ville'] ; ?>"><td/></tr>
 
-          echo '<label for="mobile">mobile</label><br>';
-          echo '<input type="text" name="mobile" value="'.$donnee['mobile'].'"><br/>';
+                              <?php if(isset($error['mobile'])){ echo '<tr><td>Erreur </td><td>'.$error['mobile'].'</td></tr>'; } ?>
+                              <tr><td><label for="mobile">mobile</label></td>
+                              <td><input type="text" name="mobile" value="<?php echo $donnee['mobile'] ; ?>"><td/></tr>
 
-          echo '<label for="fix">fix</label><br>';
-          echo '<input type="text" name="fix" value="'.$donnee['fix'].'"><br/>';
+                              <?php if(isset($error['fix'])){ echo '<tr><td>Erreur </td><td>'.$error['fix'].'</td></tr>'; } ?>
+                              <tr><td><label for="fix">fix</label></td>
+                              <td><input type="text" name="fix" value="<?php echo $donnee['fix'] ; ?>"><td/></tr>
 
-          echo '<label for="avatar">avatar</label><br>';
-          echo '<input type="text" name="avatar" value="'.$donnee['avatar'].'"><br/>';
-          echo '</form>';
-        }else{ ?>
-          <div class="container">
-            <div class="row alignement">
-              <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-6 toppad" >
-                <div class="panel panel-info">
-                  <div class="panel-heading"><?php
-                    echo '<h3 classe"userpseudo">Votre Pseudo  : '.$donnee['pseudo'].'</h3><br/>'; ?>
-                  </div>
-                  <div class="panel-body">
-                    <div class="row">
-                      <div class="col-md-12 col-lg-12 " align="center">
-                        <!-- <img alt="User Pic" src="<?php //echo $donnee['avatar'] ; ?>" class="img-circle img-responsive"> -->
-                        <img alt="User Pic" src="https://cdn.pixabay.com/photo/2012/04/13/21/07/user-33638_960_720.png"
-                        class="img-circle img-responsive col-md-3">
-                        <div class=" col-md-9 col-lg-9 cartevisite">
-                          <table class="table table-user-information">
-                            <tbody>
-                              <tr><td>Nom:</td><td><?php echo $donnee['nom'] ; ?></td></tr>
-                              <tr><td>Prénom</td><td><?php echo $donnee['prenom'] ; ?></td></tr>
-                              <tr><td>Email</td><td><a href="mailto:<?php echo $donnee['mail'] ; ?>"><?php echo $donnee['mail'] ; ?></a></td></tr>
-                              <tr><td>Adresse</td><td><?php echo $donnee['adresse'] ; ?></td></tr>
-                              <tr><td>Code postal</td><td><?php echo $donnee['code_postal'] ; ?></td></tr>
-                              <tr><td>Ville</td><td><?php echo $donnee['ville'] ; ?></td></tr> <?php
-                              echo (empty($donnee['fix']))? '<tr><td>Téléphone : </td><td>Non renseigné</td></tr>' : '<tr><td>Téléphone :</td><td> '.$donnee['fix'].'</td></tr>';
-                              echo (empty($donnee['mobile']))? '<tr><td>Portable : </td><td>Non renseigné</td></tr>' : '<tr><td>Portable :</td><td> '.$donnee['mobile'].'</td></tr>'; ?>
-                            </tbody>
-                          </table><?php
-                          if(isset($edition) && !isset($acces)){
-                            echo '<input type="submit" name="submit" value="Enregistrer"><br/>';
-                            echo '</form>';
-                          }else{
+                          </tbody>
+                        </table>
+                        <input type="submit" name="submit" value="Enregistrer">
+                      </form>
+                    </div> <?php
+                          }else{ ?>
+                            <tr><td>Nom:</td><td><?php echo $donnee['nom'] ; ?></td></tr>
+                            <tr><td>Prénom</td><td><?php echo $donnee['prenom'] ; ?></td></tr>
+                            <tr><td>Email</td><td><a href="mailto:<?php echo $donnee['mail'] ; ?>"><?php echo $donnee['mail'] ; ?></a></td></tr>
+                            <tr><td>Adresse</td><td><?php echo $donnee['adresse'] ; ?></td></tr>
+                            <tr><td>Code postal</td><td><?php echo $donnee['code_postal'] ; ?></td></tr>
+                            <tr><td>Ville</td><td><?php echo $donnee['ville'] ; ?></td></tr> <?php
+                            echo (empty($donnee['fix']))? '<tr><td>Téléphone </td><td>Non renseigné</td></tr>' : '<tr><td>Téléphone </td><td> '.$donnee['fix'].'</td></tr>';
+                            echo (empty($donnee['mobile']))? '<tr><td>Portable </td><td>Non renseigné</td></tr>' : '<tr><td>Portable </td><td> '.$donnee['mobile'].'</td></tr>';
+
+                        ?></tbody>
+                        </table>
+                      </div><?php
                             if(!isset($acces)){
                               echo '<span class="centrer"><a href="'.$this->url('admin_monCompte_edition').'"><button >EDITER</button></a>';
                               echo '<a href="'.$this->url('admin_monCompte_edition').'"><button >SUPPRIMER</button></a></span>';
                             }
                           } ?>
-                        </div>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div> <?php
-        }
+          </div>
+        </div>
+      </div> <?php
   }else{
     echo '<p>'.$donnee.'</p>';
   }
